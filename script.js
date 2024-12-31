cdfbae0dda37942eeba6a696b960fd9a9ff08c25
Ethan Saunders <essaund2@ncsu.edu>
2024-12-31 00:08:27 -0500

    [Documentation] Added comments

--- script.js
+++ script.js
@@ -101,22 +101,24 @@ function shuffle() {
 
 // Function to deal out cards to start the round
 async function deal() {
+    // Get cards from the deck and push them to the player and dealer's hand
     playerHand[0] = [];
     playerHand[0].push(deck.pop());
     dealerHand.push(deck.pop());
     playerHand[0].push(deck.pop());
     dealerHand.push(deck.pop());
 
+    // Calculate the count
     calculateCount(playerHand[0][0]);
     calculateCount(playerHand[1]);
     calculateCount(dealerHand[0][1]);
     calculateCount(dealerHand[1]);
 
+    // Add card values to totals
     playerTotal = playerHand[0][0].val + playerHand[0][1].val;
     dealerTotal = dealerHand[0].val + dealerHand[1].val;
-    //player1.innerHTML += ` <img src=${playerHand[0][0].img}> <img src=${playerHand[0][1].img}>`;
-    //dealer.innerHTML += ` <img src=${dealerHand[0].img}>`;
 
+    // Delay cards showing up on screen
     setTimeout(() => {
         player1.innerHTML += ` <img src=${playerHand[0][0].img}>`;
     }, 1000);
@@ -135,6 +137,7 @@ async function deal() {
             resolve();
         }, 4000);
 
+        // If player gets blackjack end game
         if (playerTotal === 21) {
             total += bet * 1.5;
             gamePlay = false;
@@ -146,6 +149,7 @@ async function deal() {
             result.innerHTML = "You Win!";
         }
     
+        // If players cards aren't the same disable split button
         if (playerHand[0][0].val != playerHand[0][1].val) {
             splitBtn.disable = true;
             splitBtn.style.opacity = "50%";
@@ -155,6 +159,7 @@ async function deal() {
             }
         }
     
+        // If dealer gets 21 end the game
         if (dealerTotal === 21) {
             dealer.innerHTML += ` <img src=${dealerHand[1].img}>`;
             endRound();
@@ -171,10 +176,12 @@ async function dealerRound() {
     gamePlay = false;
     let ms = 2000;
     let i = 0;
+    // Reveal other dealer card
     dealer.innerHTML = ` <img src=${dealerHand[0].img}> <img src=${dealerHand[1].img}>`;
     while (dealerPlay) {
-        
+        // Add cards to dealer hand until total reaches at least 17
         while (dealerTotal < 17) {
+            // Allow a delay between each card
             await new Promise((resolve) => {
                 let newCard = deck.pop();
                 dealerHand.push(newCard);
@@ -189,7 +196,7 @@ async function dealerRound() {
                 }, ms);
             });
         }
-
+        // If dealer total is greater than 21 and has an ace subtract 10 from the total
         if (dealerTotal > 21) {
             let aceAdjusted = false;
             for (let j = 0; j < dealerHand.length; j++) {
@@ -207,10 +214,11 @@ async function dealerRound() {
             dealerPlay = false; 
         }
     }
+    // Check if dealer busts
     if (dealerTotal > 21) {
         dealerBust = true;
     }
-
+    // Allow a delay before ending the round
     setTimeout(endRound, 1000);
 }
 
@@ -219,8 +227,9 @@ async function dealerRound() {
 function endRound() {
     gamePlay = false;
     dealerPlay = false;
+    // Check if user split cards
     if (split) {
-        if (bust && bust2) {
+        if (bust && bust2) { // Check if either hand busted
             total -= bet * 2;
             bust = false;
             bust2 = false;
@@ -237,7 +246,7 @@ function endRound() {
                 result.innerHTML = "You Win!";
             }
             dealerBust = false;
-        } else {
+        } else { // Check if player beat the dealer on each hand
             if (playerTotal2 > dealerTotal && !bust2) {
                 total += bet;
                 result.innerHTML += "Win Hand 1 ";
@@ -254,7 +263,7 @@ function endRound() {
             } 
         }
 
-    } else {
+    } else { // If user did not split check the one hand
         if (bust) {
             total -= bet;
             bust = false;
@@ -276,6 +285,7 @@ function endRound() {
             }
         }
     }
+    // Reset values and buttons
     updateTotal();
     playerHand = [];
     dealerHand = [];
@@ -310,15 +320,18 @@ newGameBtn.addEventListener("click", shuffle);
 // Even click the bet button
 // Takes bet and starts the round by dealing the cards
 betBtn.addEventListener("click", () => {
+    // Only allow button to work when user and dealer are not in action
     if (!dealerPlay && !gamePlay) {
+        // Reset board
         player1.innerHTML = "";
         player2.innerHTML = "";
         dealer.innerHTML = "";
         result.innerHTML = "";
         split = false;
-        if (deck.length <= 15) {
+        if (deck.length <= 15) { // If deck is too low reset the deck
             shuffle();
         }
+        // Get the bet value and set the user gameplay to true
         dealerHeader.innerHTML = "Dealer";
         amount.disabled = true;
         gamePlay = true;
@@ -333,18 +346,18 @@ betBtn.addEventListener("click", () => {
 // Can only be clicked when round is active
 // Adds a card to the players hand
 hitBtn.addEventListener("click", () => {
-    if (gamePlay) {
+    if (gamePlay) { // Only allow button to be used on players turn
         ddBtn.style.opacity = "50%";
         ddBtn.disabled = true;
-        if (split) {
-            if (turn === 0) {
-                let newCard = deck.pop();
+        if (split) { // Check if user split cards
+            if (turn === 0) { // Check which hand the turn is on
+                let newCard = deck.pop(); // Add card to hand and update count
                 playerHand[0].push(newCard);
                 calculateCount(newCard.val);
                 updateTotal();
                 playerTotal += newCard.val;
                 player1.innerHTML += ` <img src=${newCard.img}>`;
-                if (playerTotal > 21) {
+                if (playerTotal > 21) { // If player bust check hand for ace
                     for (let i = 0; i < playerHand[0].length; i++) {
                         if (playerHand[0][i].val === 11) {
                             playerHand[0][i].val = 1;
@@ -352,7 +365,7 @@ hitBtn.addEventListener("click", () => {
                             break;
                         }
                     }
-                    if (playerTotal > 21) {
+                    if (playerTotal > 21) { // If player bust set the turn to the next hand
                         turn = 1;
                         bust = true;
                     }
@@ -375,12 +388,12 @@ hitBtn.addEventListener("click", () => {
                             break;
                         }
                     }
-                    if (playerTotal > 21) {
+                    if (playerTotal > 21) { 
                         gamePlay = false;
                         bust2 = true;
-                        if (bust) {
+                        if (bust) { // If both hands bust end the round
                             endRound();
-                        } else {
+                        } else { // If only this hand bust let the dealer play
                             dealerPlay = true;
                             dealerRound();
                         }
@@ -391,7 +404,7 @@ hitBtn.addEventListener("click", () => {
                     dealerRound();
                 }
             }
-        } else {
+        } else { // If the cards are not split play the one hand
             splitBtn.style.opacity = "50%";
             splitBtn.disabled = true;
 
@@ -409,13 +422,13 @@ hitBtn.addEventListener("click", () => {
                         break;
                     }
                 }
-                if (playerTotal > 21) {
+                if (playerTotal > 21) { // If player busts end the round
                     bust = true;
                     gamePlay = false;
                     endRound();
                 }
             }
-            if (playerTotal === 21) {
+            if (playerTotal === 21) { // If player gets 21 let the dealer play
                 dealerPlay = true;
                 dealerRound();
             }
@@ -429,14 +442,14 @@ hitBtn.addEventListener("click", () => {
 // Moves to dealers turn
 standBtn.addEventListener("click", () => {
     if (gamePlay) {
-        if (split) {
+        if (split) { // If player split change the hand or let the dealer play
             if (turn === 0) {
                 turn = 1;
             } else {
                 dealerPlay = true;
                 dealerRound();
             }
-        } else {
+        } else { // Let the dealer play
             dealerPlay = true;
             dealerRound();
         }
@@ -447,9 +460,9 @@ standBtn.addEventListener("click", () => {
 // Add one card then end the round
 ddBtn.addEventListener("click", () => {
     if (gamePlay) {
-        bet = bet * 2;
+        bet = bet * 2; // Double the bet
 
-        let newCard = deck.pop();
+        let newCard = deck.pop(); // Add a card to hand then let the dealer play
         playerHand[0].push(newCard);
         calculateCount(newCard.val);
         updateTotal();
@@ -464,7 +477,7 @@ ddBtn.addEventListener("click", () => {
                     break;
                 }
             }
-            if (playerTotal > 21) {
+            if (playerTotal > 21) { // If hand busts end the round
                 bust = true;
                 gamePlay = false;
                 endRound();
@@ -480,14 +493,17 @@ ddBtn.addEventListener("click", () => {
 // Event click split button
 splitBtn.addEventListener("click", () => {
     if (gamePlay) {
+        // Split the two cards into seperate hands inside the player hand
         playerHand[1] = [];
         playerHand[1][0] = playerHand[0].pop();
         player1.innerHTML = ` <img src=${playerHand[0][0].img}>`;
         player2.innerHTML = ` <img src=${playerHand[1][0].img}>`;
 
+        // Update the totals
         playerTotal = playerHand[0][0].val;
         playerTotal2 = playerHand[1][0].val;
 
+        // Deal new cards to the hands and update totals
         playerHand[0].push(deck.pop());
         playerHand[1].push(deck.pop());
         playerTotal += playerHand[0][1].val;
@@ -495,7 +511,8 @@ splitBtn.addEventListener("click", () => {
 
         player1.innerHTML += ` <img src=${playerHand[0][1].img}>`;
         player2.innerHTML += ` <img src=${playerHand[1][1].img}>`;
-
+        
+        // Set split value to true and disable split button
         split = true;
         splitBtn.opacity = "50%";
         splitBtn.disabled = true;
